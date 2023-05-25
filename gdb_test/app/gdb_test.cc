43fa180a85f755cbfae71fa621b9691f77b283b7
Zhao Hongfeng <hongfeng.zhao@semidrive.com>
2023-05-25 11:16:55 +0800

    update

---
+++ gdb_test/app/gdb_test.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "gdb_test start" << endl;
+    int a, b, c;
+    a = 10;
+    b = 20;
+    c = a + b;
+    cout << "c = " << c << endl; 
+    return 0;
+}
